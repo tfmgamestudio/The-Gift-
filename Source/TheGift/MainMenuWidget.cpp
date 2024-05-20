2858ecd9bed813a7f0f8c0c1ec9303ee3e5f3d1d
ladjam96 <laura.adjam@live.com>
2024-05-20 19:25:56 +0200

    Main Menu C++

---
+++ Source/TheGift/MainMenuWidget.cpp
@@ -0,0 +1,8 @@
+#include "MainMenuWidget.h"
+
+void UMainMenuWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	//SettingsButtonWidget.SetMain
+}
---
+++ Source/TheGift/MainMenuWidget.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenuWidget.generated.h"
+
+UCLASS()
+class THEGIFT_API UMainMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	
+	// Principal buttons
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget)) UUserWidget* MainButtonWidget = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget)) UUserWidget* SettingsButtonWidget = nullptr;
+
+	// // Secondary buttons
+	// UPROPERTY(BlueprintReadWrite, meta = (BindWidget)) UUserWidget* NewGameButton = nullptr;
+	// UPROPERTY(BlueprintReadWrite, meta = (BindWidget)) UUserWidget* LoadGameButton = nullptr;
+	// UPROPERTY(BlueprintReadWrite, meta = (BindWidget)) UUserWidget* SettingsGameButton = nullptr;
+	// UPROPERTY(BlueprintReadWrite, meta = (BindWidget)) UUserWidget* SettingsVideoButton = nullptr;
+	// UPROPERTY(BlueprintReadWrite, meta = (BindWidget)) UUserWidget* SettingsSoundButton = nullptr;
+	// UPROPERTY(BlueprintReadWrite, meta = (BindWidget)) UUserWidget* SettingsControlsButton = nullptr;
+
+protected:
+
+	virtual void NativeConstruct() override;
+};

4576a7bdd1ed89f871711920579fa2d580a197c0
ladjam96 <laura.adjam@live.com>
2024-05-22 20:03:12 +0200

    Added additional raycast so player can't be detected behind objects part2

--- Source/TheGift/AIMonsterController.cpp
+++ Source/TheGift/AIMonsterController.cpp
@@ -5,7 +5,10 @@
 #include "Runtime/Engine/Public/EngineUtils.h"
 #include <Kismet/KismetSystemLibrary.h>
 #include <Kismet/GameplayStatics.h>
+
+#include "Components/CapsuleComponent.h"
 #include "GameFramework/Character.h"
+#include "Navigation/PathFollowingComponent.h"
 
 AAIMonsterController::AAIMonsterController()
 {
@@ -61,54 +64,91 @@ void AAIMonsterController::UpdateNextTargetPoint()
 
 void AAIMonsterController::CheckNearbyEnemy()
 {
-    if(HasSpawned)
+    if (HasSpawned)
     {
-        APawn* pPawn = GetPawn();
+        APawn* AIPawn = GetPawn();
+        ACharacter* PlayerCharacter = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+        
+        FVector SphereCastStart = AIPawn->GetActorLocation();
+        FVector SphereCastEnd = SphereCastStart + FVector(0, 0, 15.f); // Adjust as needed
 
-        FVector MSStart = pPawn->GetActorLocation();
-        FVector MSEnd = MSStart + FVector(0, 0, 15.f);
-
+        // Perform sphere cast to detect nearby enemies
         TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
         ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECC_Pawn));
 
         TArray<AActor*> ActorsToIgnore;
-        ActorsToIgnore.Add(pPawn);
+        ActorsToIgnore.Add(AIPawn);
 
-        TArray<FHitResult> OutHits;
+        TArray<FHitResult> SphereCastHits;
 
-        bool bResult = UKismetSystemLibrary::SphereTraceMultiForObjects(GetWorld(), MSStart, MSEnd, 500.f, ObjectTypes, false, ActorsToIgnore, EDrawDebugTrace::ForDuration, OutHits, true);
+        bool bSphereCastResult = UKismetSystemLibrary::SphereTraceMultiForObjects(GetWorld(), SphereCastStart, SphereCastEnd, 1000.f, ObjectTypes, false, ActorsToIgnore, EDrawDebugTrace::ForDuration, SphereCastHits, true);
 
         UBlackboardComponent* BlackboardComponent = BrainComponent->GetBlackboardComponent();
 
-        if (bResult)
+        if (bSphereCastResult)
         {
-            for (int32 i = 0; i < OutHits.Num(); i++)
+            for (const FHitResult& SphereCastHit : SphereCastHits)
             {
-                FHitResult Hit = OutHits[i];
-
-                ACharacter* pCharacter = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
-
-                if (Hit.GetActor() == pCharacter)
+                if (SphereCastHit.GetActor() == PlayerCharacter)
                 {
-                    BlackboardComponent->SetValueAsObject("TargetActorToFollow", pCharacter);
-                    MoveToEnemy();
+                    FVector LineTraceStart = AIPawn->GetActorLocation();
+                    FVector LineTraceEnd = PlayerCharacter->GetCapsuleComponent()->GetComponentLocation();
+
+                    FHitResult LineTraceHit;
+                    FCollisionQueryParams CollisionParams;
+                    CollisionParams.AddIgnoredActor(AIPawn);
+
+                    bool bLineTraceResult = GetWorld()->LineTraceSingleByChannel(LineTraceHit, LineTraceStart, LineTraceEnd, ECC_Pawn, CollisionParams);
+
+                    if (bLineTraceResult && LineTraceHit.GetActor() == PlayerCharacter)
+                    {
+                        BlackboardComponent->SetValueAsObject("TargetActorToFollow", PlayerCharacter);
+                        UE_LOG(LogTemp, Log, TEXT("Player detected and visible, moving to enemy."));
+                        MoveToEnemy();
+                        return; 
+                    }
+                    else
+                    {
+                        // Player not hit by line trace
+                        BlackboardComponent->SetValueAsObject("TargetActorToFollow", nullptr);
+                        UE_LOG(LogTemp, Log, TEXT("Player detected and not visible."));
+                        break;
+                    }
                 }
             }
         }
-        else
-        {
-            BlackboardComponent->SetValueAsObject("TargetActorToFollow", nullptr);
-        }
+
+        // No player detected or not visible
+        BlackboardComponent->SetValueAsObject("TargetActorToFollow", nullptr);
+        UE_LOG(LogTemp, Log, TEXT("No nearby enemies detected or player not visible."));
     }
 }
 
+
 EPathFollowingRequestResult::Type AAIMonsterController::MoveToEnemy()
 {
-        UBlackboardComponent* BlackboardComponent = BrainComponent->GetBlackboardComponent();
+    UBlackboardComponent* BlackboardComponent = BrainComponent->GetBlackboardComponent();
 
-        AActor* HeroCharacterActor = Cast<AActor>(BlackboardComponent->GetValueAsObject("TargetActorToFollow"));
+    AActor* HeroCharacterActor = Cast<AActor>(BlackboardComponent->GetValueAsObject("TargetActorToFollow"));
 
-        EPathFollowingRequestResult::Type MoveToActorResult = MoveToActor(HeroCharacterActor);
+    if (!HeroCharacterActor)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("MoveToEnemy called but TargetActorToFollow is not set."));
+        return EPathFollowingRequestResult::Type::Failed;
+    }
 
-        return MoveToActorResult;
+    UE_LOG(LogTemp, Log, TEXT("Attempting to move to actor: %s"), *HeroCharacterActor->GetName());
+
+    EPathFollowingRequestResult::Type MoveToActorResult = MoveToActor(HeroCharacterActor);
+
+    if (MoveToActorResult == EPathFollowingRequestResult::Type::RequestSuccessful)
+    {
+        UE_LOG(LogTemp, Log, TEXT("MoveToActor request successful."));
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("MoveToActor request failed."));
+    }
+
+    return MoveToActorResult;
 }

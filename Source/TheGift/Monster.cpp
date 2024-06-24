7bf013330f59487390956c26c96177e13710c4ec
ladjam96 <laura.adjam@live.com>
2024-06-24 18:12:10 +0200

    Footsteps

--- Source/TheGift/Monster.cpp
+++ Source/TheGift/Monster.cpp
@@ -1,4 +1,7 @@
 #include "Monster.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "AIMonsterController.h"
 
 AMonster::AMonster()
 {
@@ -20,4 +23,36 @@ void AMonster::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 }
 
+void AMonster::Move(const FVector2D& Value)
+{
+	MovementVector = Value;
 
+	AAIMonsterController* AIController = Cast<AAIMonsterController>(GetController());
+	if (AIController != nullptr)
+	{
+		const FRotator Rotation = AIController->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+FVector AMonster::GetCharacterVelocity() const
+{
+	return GetCharacterMovement()->Velocity;
+}
+
+FRotator AMonster::GetCharacterDirection() const
+{
+	AAIMonsterController* AIController = Cast<AAIMonsterController>(GetController());
+	if (AIController != nullptr)
+	{
+		const FRotator Rotation = AIController->GetControlRotation();
+		return FRotator(0.0f, Rotation.Yaw, 0.0f);
+	}
+	return FRotator::ZeroRotator;
+}
--- Source/TheGift/Monster.h
+++ Source/TheGift/Monster.h
@@ -2,7 +2,6 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
-#include "Waypoint.h"
 #include "Monster.generated.h"
 
 UCLASS()
@@ -20,4 +19,19 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(BlueprintReadWrite)
+	FVector2D MovementVector{0, 0};
+	
+	UPROPERTY(BlueprintReadWrite)
+	FVector2D RotationVector{0, 0};
+
+	UFUNCTION(BlueprintCallable)
+	void Move(const FVector2D& Value);
+
+	UFUNCTION(BlueprintPure)
+	FVector GetCharacterVelocity() const;
+
+	UFUNCTION(BlueprintPure)
+	FRotator GetCharacterDirection() const;
 };

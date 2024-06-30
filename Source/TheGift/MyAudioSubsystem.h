e95e5560f2e3486c6750f9722f21ba5afe239618
Luna <diegounomasuno@gmail.com>
2024-06-30 23:23:03 +0200

    used audio subsystem to change ambient

--- Source/TheGift/MyAudioSubsystem.h
+++ Source/TheGift/MyAudioSubsystem.h
@@ -13,8 +13,52 @@ UCLASS(Blueprintable, Abstract)
 class THEGIFT_API UMyAudioSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
-	
-	UPROPERTY(EditDefaultsOnly)
-	int MasterVolume = 1;
 
+private:
+	UPROPERTY()
+	float MasterVolume = 1;
+
+	UPROPERTY()
+	float MusicVolume = 1;
+
+	UPROPERTY()
+	float SFXVolume = 1;
+
+	UPROPERTY()
+	float EnvironmentVolume = 1;
+
+	UPROPERTY()
+	float VoicesVolume = 1;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	inline void SetMasterVolume(float volume) { MasterVolume = volume; }
+
+	UFUNCTION(BlueprintCallable)
+	inline void SetMusicVolume(float volume) { MusicVolume = volume; }
+
+	UFUNCTION(BlueprintCallable)
+	inline void SetSFXVolume(float volume) { SFXVolume = volume; }
+
+	UFUNCTION(BlueprintCallable)
+	inline void SetEnvironmentVolume(float volume) { EnvironmentVolume = volume; }
+
+	UFUNCTION(BlueprintCallable)
+	inline void SetVoicesVolume(float volume) { VoicesVolume = volume; }
+
+
+	UFUNCTION(BlueprintCallable)
+	inline float GetMasterVolume() { return MasterVolume; }
+
+	UFUNCTION(BlueprintCallable)
+	inline float GetMusicVolume() { return MusicVolume; }
+
+	UFUNCTION(BlueprintCallable)
+	inline float GetSFXVolume() { return SFXVolume; }
+
+	UFUNCTION(BlueprintCallable)
+	inline float GetEnvironmentVolume() { return EnvironmentVolume; }
+
+	UFUNCTION(BlueprintCallable)
+	inline float GetVoicesVolume() { return VoicesVolume; }
 };

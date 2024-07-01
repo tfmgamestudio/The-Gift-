4315d68cfeea2052e8de894388f547ea482e5d1b
AlVaRo_RdS <alvaro_rds@hotmail.com>
2024-07-01 19:15:21 +0200

    Final touches

--- Source/TheGift/TheGiftPlayerController.cpp
+++ Source/TheGift/TheGiftPlayerController.cpp
@@ -15,7 +15,7 @@ void ATheGiftPlayerController::BeginPlay()
 		// add the mapping context so we get controls
 		Subsystem->AddMappingContext(InputMappingContext, 0);
 
-		UE_LOG(LogTemp, Warning, TEXT("BeginPlay"));
+		//UE_LOG(LogTemp, Warning, TEXT("BeginPlay"));
 
 		if (MainWidgetTemplate)
 		{
